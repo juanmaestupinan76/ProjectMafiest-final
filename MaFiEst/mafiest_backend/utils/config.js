55fa23b4fe0014c371f4e5cce8a09e157e6c49b5
JuanEst989 <hubgith562@gmail.com>
2025-09-11 13:09:22 -0500

    Update config.js

--- MaFiEst/mafiest_backend/utils/config.js
+++ MaFiEst/mafiest_backend/utils/config.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
-
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || "development",
-  DB: {
-    HOST: process.env.DB_HOST || "localhost",
-    USER: process.env.DB_USER || "postgres",
-    PASSWORD: process.env.DB_PASSWORD || "mafiest",
-    NAME: process.env.DB_NAME || "mafiest_db",
-    PORT: process.env.DB_PORT || 5432,
-  },
-  JWT_SECRET: process.env.JWT_SECRET || "super_secreto",
-  JWT_EXPIRATION: process.env.JWT_EXPIRATION || "1d",
+    PORT: process.env.PORT || 3000,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT || 5432,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRATION: process.env.JWT_EXPIRATION || '1d',
+    NODE_ENV: process.env.NODE_ENV || 'development',
 };
